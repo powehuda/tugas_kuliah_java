c95f4f5af33e60c0eca6ec5368ffcbbb50cca93d
Nurhuda <powerhudaman@gmail.com>
2016-10-02 22:06:40 +0700

    test perubahan

--- src/program_restaurant/Main.java
+++ src/program_restaurant/Main.java
@@ -5,6 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.print("Dasar Program Restaurant");
+		System.out.print("Baris Kedua");
 	}
 
 }
