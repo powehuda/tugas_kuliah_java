c94832200d27c759e4be7a7421b2746c4396b3e8
Kecoaemas <teguh.nugraha76@gmail.com>
2016-10-05 20:48:22 +0700

    test

--- src/program_restaurant/Main.java
+++ src/program_restaurant/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.print("Dasar Program Restaurant");
+		System.out.print("Dasar Program Restaurant test team 7 ");
 	}
 
 }
